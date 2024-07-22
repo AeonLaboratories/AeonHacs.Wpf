de89d81636df34abf5a5d0e609370dd8e7b209f7
Tyler Wilson <tjwilson.91@gmail.com>
2024-07-22 10:01:15 -0700

    Add Inficon gauges

---
+++ Views/InficonCDG020D.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AeonHacs.Wpf.Views;
+/// <summary>
+/// Interaction logic for InficonCDG020D.xaml
+/// </summary>
+public partial class InficonCDG020D : View
+{
+    public InficonCDG020D()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/InficonCDG025D.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AeonHacs.Wpf.Views;
+/// <summary>
+/// Interaction logic for InficonCDG025D.xaml
+/// </summary>
+public partial class InficonCDG025D : View
+{
+    public InficonCDG025D()
+    {
+        InitializeComponent();
+    }
+}
