d0ed1b8e875e724e77689e0387acd334a2c53da0
Tyler Wilson <tjwilson.91@gmail.com>
2024-08-13 12:34:57 -0700

    Add RightToLeft option to VMPressure.

--- Views/VMPressure.cs
+++ Views/VMPressure.cs
@@ -4,6 +4,15 @@ namespace AeonHacs.Wpf.Views;
 
 public class VMPressure : View
 {
+    public static readonly DependencyProperty OrderProperty = DependencyProperty.Register(
+        nameof(Order), typeof(FlowDirection), typeof(VMPressure), new PropertyMetadata(FlowDirection.LeftToRight));
+
+    public FlowDirection Order
+    {
+        get => (FlowDirection)GetValue(OrderProperty);
+        set => SetValue(OrderProperty, value);
+    }
+
     static VMPressure()
     {
         DefaultStyleKeyProperty.OverrideMetadata(typeof(VMPressure), new FrameworkPropertyMetadata(typeof(VMPressure)));
