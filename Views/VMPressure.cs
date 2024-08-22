1d86c1b3026cef54106936c269d465fc95b09af8
RoyHinkley <jim.wilson@aeonlaboratories.com>
2024-08-22 15:42:21 -0700

    Added the ability to change the low vacuum pressure sensor type.

--- Views/VMPressure.cs
+++ Views/VMPressure.cs
@@ -4,8 +4,17 @@ namespace AeonHacs.Wpf.Views;
 
 public class VMPressure : View
 {
+    public static readonly DependencyProperty LowVacuumSensorTypeProperty = DependencyProperty.Register(
+        nameof(LowVacuumSensorType), typeof(PressureSensorType), typeof(VMPressure), new PropertyMetadata(PressureSensorType.Inficon_CDG020D));
+
     public static readonly DependencyProperty DirectionProperty = DependencyProperty.Register(
         nameof(Direction), typeof(FlowDirection), typeof(VMPressure), new PropertyMetadata(FlowDirection.LeftToRight));
+
+    public PressureSensorType LowVacuumSensorType
+    {
+        get => (PressureSensorType)GetValue(LowVacuumSensorTypeProperty);
+        set => SetValue(LowVacuumSensorTypeProperty, value);
+    }
 
     public FlowDirection Direction
     {
