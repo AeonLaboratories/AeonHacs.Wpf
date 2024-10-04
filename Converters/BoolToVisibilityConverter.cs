9275faef63629d06f0a7e8649f6bd57c433727d9
RoyHinkley <jim.wilson@aeonlaboratories.com>
2024-10-04 11:29:13 -0700

    Extended Visibility conditions.

--- Converters/BoolToVisibilityConverter.cs
+++ Converters/BoolToVisibilityConverter.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 using System.Windows;
 using System.Windows.Data;
 
@@ -9,18 +7,18 @@ namespace AeonHacs.Wpf.Converters
 {
     public class BoolToVisibilityConverter : IValueConverter
     {
-        public enum FalseVisibilityBehavior { Collapsed, Hidden }
-
         public static BoolToVisibilityConverter Default = new BoolToVisibilityConverter();
-        public static BoolToVisibilityConverter Hidden = new BoolToVisibilityConverter() { FalseVisibility = FalseVisibilityBehavior.Hidden };
+        public static BoolToVisibilityConverter Hidden = new BoolToVisibilityConverter() { FalseVisibility = Visibility.Hidden };
 
-        public FalseVisibilityBehavior FalseVisibility { get; set; } = FalseVisibilityBehavior.Collapsed;
+        public Visibility TrueVisibility { get; set; } = Visibility.Visible;
+
+        public Visibility FalseVisibility { get; set; } = Visibility.Collapsed;
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool b && b)
-                return Visibility.Visible;
-            return FalseVisibility == FalseVisibilityBehavior.Hidden ? Visibility.Hidden : Visibility.Collapsed;
+                return TrueVisibility;
+            return FalseVisibility;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
