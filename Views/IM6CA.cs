8b09913b7ae61301a27b0452a3c62b2ca885da99
Jim Wilson <jim.wilson@aeonlaboratories.com>
2025-01-10 15:33:45 -0700

    Added IM6CA section.

---
+++ Views/IM6CA.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace AeonHacs.Wpf.Views;
+
+public class IM6CA : Section
+{
+    static IM6CA()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(IM6CA), new FrameworkPropertyMetadata(typeof(IM6CA)));
+    }
+}
